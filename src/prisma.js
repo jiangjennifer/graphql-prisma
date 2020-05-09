cbc5f5e930d7f4c6df233395cfc434c64178222b
Jennifer Jiang <jiangbingqianj@gmail.com>
2020-05-09 03:55:06 -0400

    integrate project

---
+++ src/prisma.js
@@ -0,0 +1,9 @@
+import { Prisma } from 'prisma-binding';
+
+const prisma = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: 'http://localhost:4466',
+});
+
+// prisma.query prisma.mutation prisma.subscription prisma.exists
+prisma.query.users(null, '{ id name email }').then((data) => console.log(data));
